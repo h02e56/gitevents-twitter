f1d6a9bebc16fe33d8c92c3e4267c7ae89992b63
Josep Boada <josep@h02e56.com>
2015-01-17 18:29:12 +0100

    config file should match final one

--- index.js
+++ index.js
@@ -1,33 +1,32 @@
-var config = require('./config');
-
 var Twitter = require('node-twitter');
 
-var twitterRestClient = new Twitter.RestClient(
-    config.CONSUMER_KEY,
-    config.CONSUMER_SECRET,
-    config.TOKEN,
-    config.TOKEN_SECRET
-);
+module.exports =  function(config){
+	
+	var config = config || require('./config');
 
-module.exports =  {
-	//just test conection works
-	test: function(cb){
-		twitterRestClient.statusesHomeTimeline({}, function(err, res) {
-		    if (err){
-		        cb('Error: ' + (err.code ? err.code + ' ' + err.message : err.message), null);
-		    } if (res){
-		        cb(null, res);
-		    }
-		});
-	},
-	//upload a new tweet
-	upload: function(data, cb){
-		twitterRestClient.statusesUpdate(data, function(err, res) {
-	        if (err){
-	            cb('Error: ' + (err.code ? err.code + ' ' + err.message : err.message), null);
-	        }if (res){
-	            cb(null, res);
-	        }
-		})
+	if (!config.twitter) {
+	    throw new Error('No twitter configuration found');
 	}
+
+	debugger;
+	var twitterRestClient = new Twitter.RestClient(
+    	config.twitter.consumer_key,
+	    config.twitter.consumer_secret,
+	    config.twitter.token,
+	    config.twitter.token_secret
+	);
+
+	return {
+		//upload a new tweet
+		sendTweet: function(data, cb){
+			twitterRestClient.statusesUpdate(data, function(err, res) {
+		        if (err){
+		            cb('Error: ' + (err.code ? err.code + ' ' + err.message : err.message), null);
+		        }if (res){
+		            cb(null, res);
+		        }
+			})
+		}
+	}
+	
 }
--- test.js
+++ test.js
@@ -1,36 +1,22 @@
-var gitupTwitter = require('./index.js');
+var gitupTwitter = require('./index.js')();
 var test = require('tape');
 var util = require('util')
 
-test('conection to twitter api works', function(t){
-	t.plan(1)
-
-	gitupTwitter.test(function(err, data){
-		var res
-		if(err) res = false
-		else res = true;
-
-		t.equal(res, true);
-	})
-});
-
-test('post twt api works', function(t){
+test('post tweet works', function(t){
 	t.plan(1)
 
 	var res = false;
 	var fakeData = {
-		status: 'test fake data'
+		status: 'test fake data2'
 	}
 
-	gitupTwitter.upload(fakeData, function(err, data){
+	gitupTwitter.sendTweet(fakeData, function(err, data){
 		if(err) {
 			console.log(err);
 		}else res=true
 		
 		t.equal(res, true);
 	})
-
-
 });
 
 
