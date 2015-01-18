4ad7345e2e1fe415e974bb8ba55c15fe679ce65b
Josep Boada <josep@h02e56.com>
2015-01-18 13:50:17 +0100

    added fake talk

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 var Twitter = require('node-twitter');
 
+var talks = {
+  'date': '2014-02-12T23:00:00.000Z',
+  'event': {
+  	'name': 'My awesome event',
+  	'date': '2015-02-12T12:00:00.000Z'	
+  },
+  'talks': [{
+    'title': 'Using the API Blueprint to generate nodejs consumers',
+    'speaker': {
+      'twitter': 'bpedro',
+      'name': 'Bruno Pedro',
+      'portrait': 'https://pbs.twimg.com/profile_images/378800000473642830/2f20ecdcd1ec41452b174d04a69e87ee.jpeg'
+    },
+    'level': 'Intermediate',
+    'language': 'en',
+    'git': '',
+    'slides': 'http://www.slideshare.net/bpedro/api-code-generation',
+    'video': 'https://vimeo.com/87488883',
+    'description': 'I\'ll show you how to generate a nodejs API consumer by using the API Blueprint (http://apiblueprint.org/) to generate code from a Postman (http://www.getpostman.com/) collection.'
+  }]
+};
+
 module.exports =  function(config){
 	
 	var config = config || require('./config');
@@ -8,7 +30,6 @@ module.exports =  function(config){
 	    throw new Error('No twitter configuration found');
 	}
 
-	debugger;
 	var twitterRestClient = new Twitter.RestClient(
     	config.twitter.consumer_key,
 	    config.twitter.consumer_secret,
