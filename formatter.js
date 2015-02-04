2ec9023a9bb364c6513e1dfbf09e9b597001bc07
Josep Boada <josep@h02e56.com>
2015-02-04 18:32:50 +0100

    removed switch statement

--- formatter.js
+++ formatter.js
@@ -1,30 +1,12 @@
 var moment = require("moment");
 var twttrtxt = require('node-twitter-text');
 
-
 module.exports = function(webhook, cb){
-	
 	if(!webhook) return cb('should provide a webhook', null)
 	
 	var label = webhook.webhook.label;
 
-	switch (label) {
-	  case 'talk proposal':
-	    formatTalks()
-	    break;
-	  case 'jobs':
-	    formatJobs()
-	    break;
-	  default:
-	    return cb("uknown label wook", null);
-	}
-	function makeid(){
-		var text = "";
-		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-		for( var i=0; i < 5; i++ )
-		    text += possible.charAt(Math.floor(Math.random() * possible.length));
-		return text;
-	}
+	doFormat(label)
 
 	function formatTalks(){
 		var talks = webhook.event.talks
@@ -33,7 +15,7 @@ module.exports = function(webhook, cb){
 			, meetupURL = webhook.event.url
 
 		talks.map(function(talk){
-			var description = 'New talk:'+ makeid();
+			var description = 'New talk: '+ makeid();
 				// 	description += ' ' + eventDate
 			// description += ' @' + talk.speaker.twitter
 			// description += ' '+ twitter.autoLink(twitter.htmlEscape('talk.title'))
@@ -41,43 +23,43 @@ module.exports = function(webhook, cb){
 			talk.description = 'Angular in deepth. router, controller, views,  ';
 				
 			description += ' ' + 'www.test.com';
-			debugger
-			var remainingCharacters = 140 - 3 - description.length;
-			console.log(remainingCharacters)
+			
+			var remainingCharacters = 140 - 3 - twttr.txt.getTweetLength(description);
+			
 			description += ' ' + talk.description.slice(0, remainingCharacters ) + ' ...'
 
-			
-			// var desc = talk.description;
-			// var formatyted = twttrtxt.autoLink('New talk http://www.meetup.com', {
-			//     urlEntities: [
-			//         {
-			//           "url": null,
-			//           "display_url": talk.title,
-			//           "expanded_url": talk.url,
-			//           "indices": [
-			//             9,
-			//             20
-			//           ]
-			//         }
-			//  ]});
-
-		
-						
-
-			//rest 3 ... chars and actual tweet to see pending chars
-			
-
-			
 			// description += '' + talk.description.slice(0, pendingChars).toLowerCase()
-			
-
 			//lets send it back to send it
 			cb(null, description)
 		})
 	}
-	
-	function formatJobs(){
-		
-	}    
+	  
+	//different formatters depending on label hook(format)
+	function doFormat(format) {
+		var formats = {
+			'talks': function talk () {
+				console.log('talk')
+			},
+			'jobs': function jobs () {
+				console.log('job')
+			}
+		}
+
+		if(typeof formats[format] != 'function'){
+			return cb("uknown label wook", null);
+		}
+
+		return formats[format]()
+	} 
+
+	//just to create a different tweet than previous
+	function makeid(){
+		var text = "";
+		var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+		for( var i=0; i < 5; i++ )
+		    text += possible.charAt(Math.floor(Math.random() * possible.length));
+		return text;
+	}
+
 
 }
